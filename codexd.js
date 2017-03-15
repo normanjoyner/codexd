545ec85cfc00edf48336173a87edf730b2f97ab3
Norman Joyner <norman.joyner@gmail.com>
2017-03-15 06:06:43 -0400

    Gracefully handle no peers having the requested snapshot

--- codexd.js
+++ codexd.js
@@ -1,8 +1,10 @@
-var fs = require("fs");
-var _ = require("lodash");
-var utils = require([__dirname, "lib", "utils"].join("/"));
+var errors = require([__dirname, "lib", "errors"].join("/"));
 var constants = require([__dirname, "lib", "constants"].join("/"));
 var persistence = require([__dirname, "persistence"].join("/"));
+var utils = require([__dirname, "lib", "utils"].join("/"));
+
+var _ = require("lodash");
+var fs = require("fs");
 
 function CodexD(options){
     var self = this;
@@ -29,18 +31,35 @@ function CodexD(options){
     });
 
     this.options.legiond.on(constants.SNAPSHOT_REQUEST, function(message){
-        if(_.has(self.volumes, message.data.id)){
+        if(!_.has(self.volumes, message.data.id)){
+            self.options.legiond.send({
+                event: [constants.SNAPSHOT_PREFIX, message.data.id].join(constants.DELIMITER),
+                data: {
+                    exists: false,
+                    id: message.data.id
+                }
+            }, message.author);
+        } else {
             self.volumes[message.data.id].create_snapshot(function(err, checksum){
                 if(!err){
                     self.options.legiond.send({
                         event: [constants.SNAPSHOT_PREFIX, message.data.id].join(constants.DELIMITER),
                         data: {
+                            checksum: checksum,
+                            exists: true,
                             id: message.data.id,
-                            persistence: self.volumes[message.data.id].type,
-                            checksum: checksum
+                            persistence: self.volumes[message.data.id].type
                         },
                         stream: fs.createReadStream([self.options.tmp_path, message.data.id].join("/"), { encoding: "base64" })
-                    });
+                    }, message.author);
+                } else {
+                    self.options.legiond.send({
+                        event: [constants.SNAPSHOT_PREFIX, message.data.id].join(constants.DELIMITER),
+                        data: {
+                            exists: false,
+                            id: message.data.id
+                        }
+                    }, message.author);
                 }
             });
         }
@@ -89,34 +108,62 @@ CodexD.prototype.get_snapshot = function(id, fn){
         }
     });
 
-    this.options.legiond.on([constants.SNAPSHOT_PREFIX, id].join(constants.DELIMITER), function(message){
+    const received_snapshot = false;
+
+    // collect all responses, timing out after 5 seconds
+    setTimeout(function() {
         self.options.legiond.leave([constants.SNAPSHOT_PREFIX, id].join(constants.DELIMITER));
+        if(!received_snapshot) {
+            return fn(new errors.ESNAPSHOTNOTFOUND());
+        }
+    }, 5000);
 
-        var stream = fs.createWriteStream([self.options.tmp_path, message.data.id].join("/"), { defaultEncoding: "binary" });
+    this.options.legiond.on([constants.SNAPSHOT_PREFIX, id].join(constants.DELIMITER), function(message){
+        // ensure we found a valid snapshot and haven't already got a response from another peer
+        if(message.data.exists && !received_snapshot) {
+            received_snapshot = true;
 
-        stream.on("error", function(err){});
+            var stream = fs.createWriteStream([self.options.tmp_path, message.data.id].join("/"), { defaultEncoding: "binary" });
 
-        stream.on("finish", function(){
-            self.create_volume({
-                id: message.data.id,
-                persistence: message.data.persistence
-            }, function(err){
-                if(err)
-                    return fn(err);
+            const stream_error = null;
 
-                self.volumes[message.data.id].restore_snapshot({
-                    checksum: message.data.checksum
-                }, fn);
+            stream.on("error", function(err){
+                if(!stream_error) {
+                    stream_error = err;
+                    stream.end();
+                }
             });
-        });
 
-        message.stream.on("data", function(data){
-            stream.write(new Buffer(data.toString(), "base64").toString("binary"), "binary");
-        });
+            stream.on("finish", function(){
+                if(stream_error) {
+                    return fn(stream_error);
+                } else {
+                    self.create_volume({
+                        id: message.data.id,
+                        persistence: message.data.persistence
+                    }, function(err){
+                        if(err)
+                            return fn(err);
 
-        message.stream.on("finish", function(){
-            stream.end();
-        });
+                        self.volumes[message.data.id].restore_snapshot({
+                            checksum: message.data.checksum
+                        }, fn);
+                    });
+                }
+            });
+
+            message.stream.on("error", function(err){
+                stream.emit("error", err);
+            });
+
+            message.stream.on("data", function(data){
+                stream.write(new Buffer(data.toString(), "base64").toString("binary"), "binary");
+            });
+
+            message.stream.on("finish", function(){
+                stream.end();
+            });
+        }
     });
 }
 
